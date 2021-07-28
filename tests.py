46782aa7a500368357e761af346efd679ba6cd79
msi-ru-cs <mohammad.s.islam@ryerson.ca>
2021-07-28 17:25:35 -0400

    Changed code for test

--- tests.py
+++ tests.py
@@ -1,5 +1,16 @@
-# print("hello")
-
 a = 2
 b = 3
 c = a + b
+
+if (c >= 5):
+    c = c + 1
+    # Swap the values of a and b
+    a = a + b
+    b = a - b
+    a = a - b
+else:
+    c = c + 10
+    temp = a
+    a = b
+    b = temp
+    
